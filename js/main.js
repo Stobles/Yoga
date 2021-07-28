19235c256963258a3a2b3e6844c5dcc8d7665136
Stobles <82175280+Stobles@users.noreply.github.com>
2021-07-28 20:02:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', (e) => {
+    let ClickTarget = e.target
+    if(ClickTarget.closest('.burger')){
+        document.querySelector('.burger').classList.toggle('active')
+        if(document.querySelector('.burger').classList.contains('active')){
+            document.querySelector('.header__navigation').style.transform = 'translateX(0%)'
+        }
+        else{
+            document.querySelector('.header__navigation').style.transform = ''
+        }
+    }
+})
---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+new Swiper('.swiper-container', {
+
+    observer: true,
+    observeParents: true,
+    spaceBetween: 20,
+    slidesPerView: 4,
+
+    navigation: {
+        nextEl: '.classes__slider--next',
+        prevEl: '.classes__slider--prev',
+      },
+
+      breakpoints:{
+        992: {
+          slidesPerView: 4
+        },
+
+        767: {
+          slidesPerView:3,
+        },
+
+        480:{
+          slidesPerView:2,
+        },
+
+        0:{
+          slidesPerView:1,
+        }
+      }
+})
